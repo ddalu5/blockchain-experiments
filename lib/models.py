4f275382882601e205652489ccca0ed97d184407
Salah OSFOR <icarus@daedalu5.org>
2017-01-12 16:00:53 +0000

    reinit block definition

--- lib/models.py
+++ lib/models.py
@@ -1,14 +1,19 @@
 import re
+import uuid
 import hashlib
+from datetime import datetime
 from settings import VALID_SIGNATURE
 
 
 class Block:
 
-    def __init__(self, block_id, data, prev_hash):
+    def __init__(self, chain_id, block_id, data, prev_hash):
+        self.__chain_id = chain_id
         self.__block_id = block_id
         self.__data = data
         self.__prev_hash = prev_hash
+        self.__created_at = datetime.utcnow()
+        self.__mine()
 
     def __mine(self):
         tmp_hash = ''
@@ -19,6 +24,7 @@ class Block:
             tmp_hash = hashlib.sha256(partial_str+str(i)).hexdigest()
             if re_object.match(tmp_hash):
                 self.__nonce = i
+                break
             i += 1
 
     def get_id(self):
@@ -33,8 +39,18 @@ class Block:
     def get_nonce(self):
         return self.__nonce
 
+    def get_datetime(self):
+        return self.__created_at
+
+    def get_chain_id(self):
+        return self.__chain_id
+
     def get_hash(self):
         return hashlib.sha256(str(self.__block_id)+str(self.__data) +
                               self.__prev_hash+str(self.__nonce)).hexdigest()
 
 
+class Blockchain:
+
+    def __init__(self):
+        self.__chain_id = uuid.uuid1()
--- lib/utils.py
+++ lib/utils.py
@@ -1,3 +1,6 @@
 import hashlib
 
 
+def get_block_hash(chain_id, block_id, data, previous_hash, nonce, created_at):
+    str_tmp = str(chain_id)+str(block_id)+str(data)+previous_hash+str(nonce)+str(created_at)
+    return
